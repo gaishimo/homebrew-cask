11d1f980c600c258fa2d83d7766e6f5e51d2951b
gaishimo <gaithimo@gmail.com>
2014-05-03 10:31:08 +0900

    .

--- Casks/sublime-text3.rb
+++ Casks/sublime-text3.rb
@@ -2,7 +2,7 @@ class SublimeText3 < Cask
   url 'http://c758482.r82.cf2.rackcdn.com/Sublime%20Text%20Build%203059.dmg'
   homepage 'http://www.sublimetext.com/3'
   version '3059'
-  sha256 no_checksum
+  no_checksum
   link 'Sublime Text.app'
   binary 'Sublime Text.app/Contents/SharedSupport/bin/subl'
   caveats do
