df3ccb80cd87da46b93b86bfe5958923afdb67f2
gaishimo <gaithimo@gmail.com>
2014-05-04 20:36:14 +0900

    Change jumpcut cask

--- Casks/jumpcut.rb
+++ Casks/jumpcut.rb
@@ -1,5 +1,5 @@
 class Jumpcut < Cask
-  url 'http://downloads.sourceforge.net/project/jumpcut/jumpcut/0.63/Jumpcut_0.63.tgz'
+  url 'http://downloads.sourceforge.net/jumpcut/Jumpcut_0.63.tgz'
   homepage 'http://jumpcut.sourceforge.net/'
   version '0.63'
   sha256 '19c84eefbc7f173af45affe3a9ca6fd9ec58d9bdf6bacef165085e63e82d54e1'
